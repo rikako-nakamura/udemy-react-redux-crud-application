f362de165ed0161618809c8501f2406db249e77b
rikako <jc180215b@gmail.com>
2024-01-08 13:00:26 +0900

    create components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React, {Component} from "react";
+import React from "react";
 
-class App extends Component{
-  render(){
-    return(
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("I am clicked")}}/>
-      </React.Fragment>
-    )
-  }
+// class App extends Component{
+// render(){
+//   return(
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => {console.log("I am clicked")}}/>
+//     </React.Fragment>
+//   )
+// }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
+const Cat = () => {
+  return <div>にゃあ</div>
+}
+
 export default App;
